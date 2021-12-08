db733e9b7f46cb39241a2cabb7209881f7f2b72e
G. Gordon Nasseri <ProphetManX@gmail.com>
2021-12-07 23:49:20 -0500

    finished 2020 day 2

---
+++ ProphetsWay.AoC.Core.Test/y2020/Day_02 Tests.cs
@@ -0,0 +1,10 @@
+using ProphetsWay.AoC.Core.y2020.Day_02;
+
+namespace ProphetsWay.AoC.Core.Test.y2020
+{
+    public class Day_02_Tests : BaseTests<Logic>
+    {
+        public override string Part1Result => "477";
+        public override string Part2Result => "686";
+    }
+}
---
+++ ProphetsWay.AoC.Core/y2020/Day_02/Logic.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProphetsWay.AoC.Core.y2020.Day_02
+{
+    public class Logic : BaseLogic
+    {
+        public override string Part1()
+        {
+            var reader = GetInputTextReader();
+
+            var valid = 0;
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                if (line == null)
+                    continue;
+
+                var parts = line.Split(" ");
+                var charConstraints = parts[0];
+                var charTarget = parts[1];
+                var password = parts[2];
+
+                var limits = charConstraints.Split("-");
+                var min = int.Parse(limits[0]);
+                var max = int.Parse(limits[1]);
+
+                var reqLetter = charTarget[0];
+                var letterCount = password.Count(x=> x == reqLetter);
+
+                if (min <= letterCount && letterCount <= max)
+                    valid++;
+            }
+
+            return valid.ToString();
+        }
+
+        public override string Part2()
+        {
+            var reader = GetInputTextReader();
+
+            var valid = 0;
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                if (line == null)
+                    continue;
+
+                var parts = line.Split(" ");
+                var charConstraints = parts[0];
+                var charTarget = parts[1];
+                var password = parts[2];
+
+                var limits = charConstraints.Split("-");
+                var min = int.Parse(limits[0]);
+                var max = int.Parse(limits[1]);
+
+                var reqLetter = charTarget[0];
+
+                if(password.Length >= max)
+                {
+                    var alpha = password[min - 1];
+                    var bravo = password[max - 1];
+
+                    if(alpha == reqLetter || bravo == reqLetter)
+                    {
+                        if (alpha == bravo)
+                            continue;
+
+                        valid++;
+                    } 
+                }
+                else
+                {
+                    var x = "what";
+                }
+            }
+
+            return valid.ToString();
+        }
+
+       
+
+    }
+}
