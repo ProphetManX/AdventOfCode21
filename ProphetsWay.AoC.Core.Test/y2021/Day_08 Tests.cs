01030498a68ab6adeac3f7917e01a3bf0dde049b
G. Gordon Nasseri <ProphetManX@gmail.com>
2021-12-08 23:19:45 -0500

    forgot to update day 8 unit test value

--- ProphetsWay.AoC.Core.Test/y2021/Day_08 Tests.cs
+++ ProphetsWay.AoC.Core.Test/y2021/Day_08 Tests.cs
@@ -5,6 +5,6 @@ namespace ProphetsWay.AoC.Core.Test.y2021
     public class Day_08_Tests : BaseTests<Logic>
     {
         public override Results Part1 => new Results("26", "344");
-        public override Results Part2 => new Results("61229", "TEST");
+        public override Results Part2 => new Results("61229", "1048410");
     }
 }
