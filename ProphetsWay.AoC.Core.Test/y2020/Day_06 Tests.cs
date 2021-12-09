52972721e46019f5c84cd93e61f98aa6abc37f78
G. Gordon Nasseri <ghnasser@microsoft.com>
2021-12-09 14:42:42 -0500

    finished 2020 day 6

---
+++ ProphetsWay.AoC.Core.Test/y2020/Day_06 Tests.cs
@@ -0,0 +1,10 @@
+using ProphetsWay.AoC.Core.y2020.Day_06;
+
+namespace ProphetsWay.AoC.Core.Test.y2020
+{
+    public class Day_06_Tests : BaseTests<Logic>
+    {
+        public override Results Part1 => new Results("11", "6775");
+        public override Results Part2 => new Results("6", "3356");
+    }
+}
---
+++ ProphetsWay.AoC.Core/y2020/Day_06/Logic.cs
@@ -0,0 +1,102 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace ProphetsWay.AoC.Core.y2020.Day_06
+{
+    public class Logic : BaseLogic
+    {
+        public List<Group> LoadGroups(bool runSample)
+        {
+            var reader = GetInputTextReader(runSample);
+
+            var groups = new List<Group>();
+            var currGroup = new Group();
+            groups.Add(currGroup);
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                if (line == null)
+                    continue;
+
+                if (string.IsNullOrEmpty(line))
+                {
+                    currGroup = new Group();
+                    groups.Add(currGroup);
+                    continue;
+                }
+
+                var aSet = new AnswerSet(line);
+                currGroup.AddPerson(aSet);
+            }
+
+            return groups;
+        }
+
+        public override string Part1(bool runSample = false)
+        {
+            var groups = LoadGroups(runSample);
+
+            var total = groups.Sum(x => x.UniqueAnswers.Count);
+            return total.ToString();
+        }
+
+        public override string Part2(bool runSample = false)
+        {
+            var groups = LoadGroups(runSample);
+
+            var total = groups.Sum(x => x.UnanimousAnswers.Count);
+            return total.ToString();
+        }
+        
+        public class Group
+        {
+            public Group()
+            {
+                People = new List<AnswerSet>();
+                UniqueAnswers = new List<char>();
+                UnanimousAnswers = new List<char>();
+            }
+
+            public List<char> UniqueAnswers { get; }
+
+            public List<char> UnanimousAnswers { get; }
+
+            public void AddPerson(AnswerSet answerSet)
+            {
+
+                foreach (var question in answerSet.PositiveAnswers)
+                {
+                    if (!UniqueAnswers.Contains(question))
+                        UniqueAnswers.Add(question);
+
+             
+                    if(People.Count == 0)
+                        UnanimousAnswers.Add(question);
+                }
+
+                var badQuestions = new List<char>();
+                foreach(var uQuestion in UnanimousAnswers)
+                    if (!answerSet.PositiveAnswers.Contains(uQuestion))
+                        badQuestions.Add(uQuestion);
+
+                foreach (var bad in badQuestions)
+                    UnanimousAnswers.Remove(bad);
+
+                People.Add(answerSet);
+            }
+
+            public List<AnswerSet> People { get; }
+        }
+
+        public class AnswerSet
+        {
+            public AnswerSet(string personalAnswers)
+            {
+                PositiveAnswers = personalAnswers;
+            }
+            
+            public string PositiveAnswers { get; }
+        }
+
+    }
+}
